c5e4569c3eaaeda062f0b7e86ce8e30d96fe5a6f
Christian Hochlin <christian.hochlin@gmail.com>
2018-12-06 21:20:45 +0100

    Corrected name of js-language, moved webpack to dev-dependency

--- App/src/components/CodeBlock.tsx
+++ App/src/components/CodeBlock.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Lowlight from 'react-lowlight';
 import javascript from 'highlight.js/lib/languages/javascript';
 import text from 'highlight.js/lib/languages/plaintext';
-Lowlight.registerLanguage('js', javascript);
+Lowlight.registerLanguage('javascript', javascript);
 Lowlight.registerLanguage('text', text);
 
 export class CodeBlock extends React.Component<{ language: string, code: string }, {}> {
